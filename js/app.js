1692a781820758ce1fd5e57fbb1f2cafb8060960
Parita Akoliya <akoliya.parita@gmail.com>
2024-09-27 13:14:06 -0400

    Add all files

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $('.header').height($(window).height());
+})
+$(document).ready(function () {
+  $('#about-header').height($(window).height());
+})
+$(document).ready(function () {
+  $('.cardio-header').height($(window).height());
+})
+$(document).ready(function () {
+  $('.treadmill-header').height($(window).height());
+})
+$(document).ready(function () {
+  $('.elliptical-header').height($(window).height());
+})
+$(document).ready(function () {
+  $('.exercisebike-header').height($(window).height());
+})
+function validateForm() {
+  alert("Your Order is submitted Successfully!")
+}
